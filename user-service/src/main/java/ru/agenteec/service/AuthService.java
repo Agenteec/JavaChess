f0b5c56b5f3487f1f0e8b3ee703dcd7c87bb37db
Ёж Соник <g.mens.hl@gmail.com>
2026-05-26 15:24:46 +0300

    Добавлена авторизация, начальный рейтинг пока будет 1200

---
+++ user-service/src/main/java/ru/agenteec/service/AuthService.java
@@ -0,0 +1,51 @@
+package ru.agenteec.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.agenteec.dto.AuthResponse;
+import ru.agenteec.dto.LoginRequest;
+import ru.agenteec.dto.RegisterRequest;
+import ru.agenteec.entity.User;
+import ru.agenteec.repository.UserRepository;
+import ru.agenteec.security.JwtService;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public AuthService(UserRepository userRepository, JwtService jwtService) {
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        if (userRepository.findByUsername(request.getUsername()).isPresent()) {
+            throw new RuntimeException("Username already taken!");
+        }
+
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRating(1200);
+
+        userRepository.save(user);
+
+        String token = jwtService.generateToken(user.getUsername());
+        return new AuthResponse(token, user.getUsername(), user.getRating());
+    }
+
+    public AuthResponse login(LoginRequest request) {
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new RuntimeException("User not found!"));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid password!");
+        }
+
+        String token = jwtService.generateToken(user.getUsername());
+        return new AuthResponse(token, user.getUsername(), user.getRating());
+    }
+}
