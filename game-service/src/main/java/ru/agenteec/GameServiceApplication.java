2cedb3d9a0df875a5cb5bd3f189aa00043c88d24
Ёж Соник <g.mens.hl@gmail.com>
2026-05-26 15:09:02 +0300

    Первоначальная настройка спринга

--- game-service/src/main/java/ru/agenteec/GameServiceApplication.java
+++ game-service/src/main/java/ru/agenteec/GameServiceApplication.java
@@ -1,10 +1,11 @@
 package ru.agenteec;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class GameServiceApplication {
     public static void main(String[] args) {
-        //SpringApplication.run(GameServiceApplication.class, args);
+        SpringApplication.run(GameServiceApplication.class, args);
     }
 }
