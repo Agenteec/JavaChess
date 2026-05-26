fbb5d510632c3a09e8793e99cbfe05796287dd56
Ёж Соник <g.mens.hl@gmail.com>
2026-05-26 15:49:54 +0300

    Логика перехода на WS

---
+++ game-service/src/main/java/ru/agenteec/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package ru.agenteec.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import ru.agenteec.websocket.GameWebSocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final GameWebSocketHandler gameWebSocketHandler;
+
+    public WebSocketConfig(GameWebSocketHandler gameWebSocketHandler) {
+        this.gameWebSocketHandler = gameWebSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(gameWebSocketHandler, "/game")
+                .setAllowedOrigins("*");
+    }
+}
